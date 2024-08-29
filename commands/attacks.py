b1a5acca8a075d05d14e69df6c0f6cccefa032d2
MilkyWay96 <vlad.kryvoruchko2010@gmail.com>
2024-08-29 14:22:50 +0300

    More attack info

--- commands/attacks.py
+++ commands/attacks.py
@@ -3,20 +3,18 @@ from aiogram.types import *
 from aiogram import Router
 
 import config
+import coc
 from client import client
 
 
 router = Router()
 
 
-async def get_attack_data(clan_tag: str) -> list:
+async def get_attack_data(war: coc.ClanWar) -> list:
     entries = []
-
-    last_war = await client.get_clan_war(clan_tag)
-
-    for player in last_war.members:
+    for player in war.members:
         if not player.is_opponent:
-            entry = (player.name, player.attacks, player.star_count)
+            entry = (player.map_position, player.name, player.attacks, player.star_count)
             entries.append(entry)
 
     return entries
@@ -24,9 +22,10 @@ async def get_attack_data(clan_tag: str) -> list:
 
 @router.message(Command("атаки"))
 async def handler(message: Message) -> None:
-    entries = await get_attack_data(config.CLAN_TAG)
-    string = ""
+    war = await client.get_clan_war(config.CLAN_TAG)
+    entries = await get_attack_data(war)
+    string = f"<b>{war.clan.name}</b>  <i>проти</i>  <b>{war.opponent.name}</b>\n"
     for entry in entries:
-        string += f"<b>{str(len(entry[1]))}⚔️ {str(entry[2])}⭐</b> - {entry[0]}\n"
+        string += f"<b>{str(len(entry[2]))}⚔️  {str(entry[3])}⭐</b>  -  {str(entry[0])}. {entry[1]}\n"
     
     await message.answer(string)
